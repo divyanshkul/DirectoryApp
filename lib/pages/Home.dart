c120d8352e2b1eab9d38120c622612e9fd5cab22
Raghav S <sitaraman.raghav.india@gmail.com>
2021-04-13 02:11:22 +0530

    Firebase shit

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:directory_app/pages/Locate.dart';
 import 'package:directory_app/pages/Chat.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -13,26 +14,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body:
-      Container(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-
         color: Colors.grey[900],
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: <Widget>[
             Text(
-                "Something",
+              "Something",
             ),
           ],
         ),
-
       ),
-
-
     );
   }
 }
