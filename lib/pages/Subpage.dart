13740ef9d72e00bc9476c03b3399618cac4b6349
Raghav S <sitaraman.raghav.india@gmail.com>
2021-04-13 03:03:18 +0530

    Firebase implementedd

--- lib/pages/Subpage.dart
+++ lib/pages/Subpage.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
 class Subpage extends StatefulWidget {
   @override
@@ -8,17 +9,40 @@ class Subpage extends StatefulWidget {
 }
 
 class _SubpageState extends State<Subpage> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  // final _auth = FirebaseAuth.instance;
+  final testRef = FirebaseDatabase.instance.reference().child("test");
 
 
   @override
   Widget build(BuildContext context) {
     return Stack(
+
       children: [
         GoogleMap(
+          mapType: MapType.normal,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          zoomGesturesEnabled: true,
+          zoomControlsEnabled: true,
           initialCameraPosition:
               CameraPosition(target: LatLng(24.142, -110.321), zoom: 15),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.blueAccent,
+              child: TextButton(
+                  child: Text("Click me to append"),
+                  onPressed: (){
+                    testRef.set("LAWDA");
+                  },
+              ),
+            ),
+          ],
         )
+
       ],
     );
   }
