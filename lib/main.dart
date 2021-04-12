83977ba074ac00fdeaa30e4049007c7bcd4912d5
Divyansh Kulshreshtha <k.divyansh268@gmail.com>
2021-04-13 02:12:04 +0530

    Added cards on homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,39 @@
-import 'dart:js';
-
 import 'package:directory_app/pages/Subpage.dart';
 import 'package:flutter/material.dart';
 import 'package:directory_app/pages/Chat.dart';
 import 'package:directory_app/pages/Home.dart';
 import 'package:directory_app/pages/Locate.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:directory_app/pages/loading.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:directory_app/pages/Welcome.dart';
-import 'package:directory_app/pages/Subpage.dart';
-import 'package:directory_app/pages/SubpageMap.dart';
-import 'package:directory_app/pages/loading.dart';
+// import 'package:directory_app/pages/Subpage.dart';
+// import 'package:directory_app/pages/SubpageMap.dart';
+// import 'package:directory_app/pages/loading.dart';
+
 //void main() => runApp(MyApp());
-void main() => runApp(MaterialApp(
-  initialRoute: Hom,
-  routes: {
-    '/Welcome': (context) => Welcome(),
-    '/Home': (context) => Home(),
-    '/Chat': (context) => Chat(),
-    '/SubpageMap': (context) => Subpage(),
-    '/Locate': (context) => Locate(),
-    '/Subpage': (context) => Subpage(),
-    '/loading': (context)=> Loading(),
-    //base class what opens  in the beginnering, context means abhi ya hai
-  },
-));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(DirectoryApp());
+}
 
+class DirectoryApp extends StatelessWidget{
 
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Manipsal Directory',
-//       theme: ThemeData(
-//         primarySwatch: Colors.grey,
-//       ),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-// class _MyHomePageState extends State<MyHomePage> {
-//
-//   int selectedpage = 0; //initial value
-//
-//   final _pageOptions = [Home(), Locate(), Chat()]; // listing of all 3 pages index wise
-//
-//   final bgcolor = [Colors.grey[900], Colors.grey[900], Colors.grey[900]];  // changing color as per active index value
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: _pageOptions[selectedpage], // initial value is 0 so HomePage will be shown
-//       bottomNavigationBar: CurvedNavigationBar(
-//         height: 50,
-//         buttonBackgroundColor: Colors.white,
-//         backgroundColor: bgcolor[selectedpage],
-//         color: Colors.white,
-//         animationCurve: Curves.linearToEaseOut,
-//         items: <Widget>[
-//           Icon(
-//             Icons.contacts,
-//             size: 30,
-//             color: Colors.purple,
-//           ),
-//           Icon(
-//             Icons.map,
-//             size: 30,
-//             color: Colors.purple,
-//           ),
-//           Icon(
-//             Icons.chat_bubble,
-//             size: 30,
-//             color: Colors.purple,
-//           )
-//         ],
-//         onTap: (index) {
-//           setState(() {
-//             selectedpage = index;  // changing selected page as per bar index selected by the user
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/Welcome',
+      routes: {
+        '/Welcome': (context) => Welcome(),
+        '/Home': (context) => Home(),
+        '/Chat': (context) => Chat(),
+        // '/SubpageMap': (context) => SubpageMap(),
+        '/Locate': (context) => Locate(),
+        '/Subpage': (context) => Subpage(),
+        '/loading': (context) => Loading(),
+        //base class what opens  in the beginnering, context means abhi ya hai
+      },
+    );
+  }
+}
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:directory_app/pages/Locate.dart';
 import 'package:directory_app/pages/Chat.dart';
+import 'package:directory_app/services/CardData.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -13,26 +16,52 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body:
-      Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-
-        color: Colors.grey[900],
+      backgroundColor: Colors.grey[800],
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
           children: <Widget>[
-            Text(
-                "Something",
+            SizedBox(
+              height: 10,
             ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        "Directory",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(
+              color: Colors.grey[200],
+              thickness: 0.3,
+            ),
+
+            CardData()
           ],
         ),
-
       ),
-
-
     );
   }
 }
